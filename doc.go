32e1f468514b763ece2639319fa2003b26f342ef
Víctor de Pedraza <v.depedraza@nayarsystems.com>
2024-10-02 10:35:02 +0200

    update example to schema v2

--- doc.go
+++ doc.go
@@ -3,52 +3,59 @@ Package bstates implements a parser for Idefix event blobs.
 
 # Overview
 
-Each binary blob is an event queues which is composed of one or more state encoded using the same schema. States represent a system state in a point of time. Schemas are json files which define States binary formats. Here there is an example schema which defines a 116bit [State] composed of 4 fields:
+Each binary blob is an event queue which is composed of one or more state encoded using the same schema. States represent a system state in a point of time. Schemas are json files which define States binary formats. Here there is an example schema which defines a 102bit [State] composed of 4 fields:
 
-	{
-	    "fields": [
-	        {
-	            "name": "3BITS_INT",
-	            "type": "int",
-	            "size": 3
-	        },
-	        {
-	            "name": "STATE_CODE",
-	            "type": "uint",
-	            "size": 2
-	        },
-	        {
-	            "name": "BOOL2",
-	            "type": "bool"
-	        },
-	        {
-	            "name": "MESSAGE_BUFFER",
-	            "type": "buffer",
-	            "size": 96
-	        }
-	    ],
-	    "decoderIntMaps":
-	    {
-	        "STATE_MAP": {
-	            "0" : "IDLE",
-	            "1" : "STOPPED",
-	            "2" : "RUNNING"
-	        }
-	    },
-	    "mappedFields":
-	    {
-	        "STATE": {
-	            "from": "STATE_CODE",
-	            "mapId": "STATE_MAP"
-	        }
-	    },
-	    "decodedFields":
-	    {
-	        "MESSAGE": {
-	            "from": "MESSAGE_BUFFER",
-	            "decoder": "BufferToString"
-	        }
-	    }
+		{
+			"version": "2.0",
+			"encoderPipeline": "t:z",
+
+			"decodedFields":
+			{
+				{
+					"name": "MESSAGE"
+					"from": "MESSAGE_BUFFER",
+					"decoder": "BufferToString"
+
+				},
+				{
+					"name": "STATE",
+					"decoder": "IntMap",
+					"params": {
+						"from": "STATE_CODE",
+						"mapId": "STATE_MAP"
+				}
+			},
+
+			"decoderIntMaps":
+				{
+					"STATE_MAP": {
+						"0" : "IDLE",
+						"1" : "STOPPED",
+						"2" : "RUNNING"
+					}
+				},
+
+		    "fields": [
+		        {
+		            "name": "3BITS_INT",
+		            "type": "int",
+		            "size": 3
+		        },
+		        {
+		            "name": "STATE_CODE",
+		            "type": "uint",
+		            "size": 2
+		        },
+		        {
+		            "name": "BOOL",
+		            "type": "bool"
+		        },
+		        {
+		            "name": "MESSAGE_BUFFER",
+		            "type": "buffer",
+		            "size": 96
+		        }
+		    ]
 	}
 
 Decoders allow for complex codification of data, those saving space. In the above example there is a decodedField MESAGE which allows us to read MESSAGE_BUFFER as a string. Decoders work by executing the "decoder" parameter on the "from" field. Currently there are three types of [Decoder].
