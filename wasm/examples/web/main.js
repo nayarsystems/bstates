0cc6e2123687abc9fbaf0094318cbbacf1aaa028
Diego Centelles Beltran <dcentelles@nayarsystems.com>
2025-01-23 11:06:36 +0100

    avoid link files

--- wasm/examples/web/main.js
+++ wasm/examples/web/main.js
@@ -1,5 +1,4 @@
 import { load as bstatesLoad } from '../../index.js';
-// import { load as bstatesLoad } from 'bstates'; // This is the import for the published package
 import { example } from '../../example.js';
 
 (async () => {
